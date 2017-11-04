378d9dcd629ef4d5207277cec6beca1ae8a625b8
Brad Chao <brad@brad.tw>
2017-11-04 13:38:14 +0800

    v2

--- app/src/main/java/com/example/administrator/myservicetest2/MainActivity.java
+++ app/src/main/java/com/example/administrator/myservicetest2/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.administrator.myservicetest2;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private MyService myService;
+    private boolean isBound;
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+            MyService.LocalBinder binder = (MyService.LocalBinder)service;
+            myService = binder.getService();
+            isBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            isBound= false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent it = new Intent(this, MyService.class);
+        bindService(it, mConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (isBound){
+            unbindService(mConnection);
+        }
     }
 }
